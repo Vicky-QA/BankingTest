91612f9b57ba392ecae727fb9fd5593572e44ea0
K Vikram Kumar <kvikramkumar@Ks-MacBook-Pro.local>
2023-04-04 12:35:19 +0530

    Added RestClient changes

--- src/test/java/com/banking/controller/BankingTest.java
+++ src/test/java/com/banking/controller/BankingTest.java
@@ -4,6 +4,7 @@ import com.banking.entity.Account;
 import com.banking.entity.Customer;
 import com.banking.repository.AccountRepository;
 import com.banking.repository.CustomerRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -35,98 +36,7 @@ public class BankingTest {
 
     private static final String baseUrl = "http://localhost:8080/banking";
 
-    @Test
-    public void depositAmountTest() {
-        Account acc = new Account();
-        acc.setAccountNum(1010101010L);
-        acc.setBalanceAmt(1000.0d);
-        acc.setAcctId(2L);
-        this.mockServer.expect(requestTo(baseUrl + "/deposit"))
-                .andRespond(withSuccess((Resource) acc, MediaType.APPLICATION_JSON));
-
-        Account res = accountRepo.save(acc);
-        assertEquals(acc.getBalanceAmt(), res.getBalanceAmt());
-        assertEquals(acc.getAcctId(), res.getAcctId());
-    }
-
-    @Test
-    public void depositAmountLimitExceptionTest() {
-        Account acc = new Account();
-        acc.setAccountNum(1010101010L);
-        acc.setBalanceAmt(1000.0d);
-        acc.setAcctId(2L);
-        this.mockServer.expect(requestTo(baseUrl + "/deposit"))
-                .andRespond(withStatus(HttpStatus.BAD_REQUEST));
-    }
-
-    @Test
-    public void withdrawAmountTest() {
-        Account acc = new Account();
-        acc.setAccountNum(1010101010L);
-        acc.setBalanceAmt(1000.0d);
-        acc.setAcctId(2L);
-        this.mockServer.expect(requestTo(baseUrl + "/withdraw"))
-                .andRespond(withSuccess((Resource) acc, MediaType.APPLICATION_JSON));
-
-        Account res = accountRepo.save(acc);
-        assertEquals(acc.getBalanceAmt() - 200, res.getBalanceAmt());
-        assertEquals(acc.getAcctId(), res.getAcctId());
-    }
-
-    @Test
-    public void withdrawAmountLimitExceptionTest() {
-        Account acc = new Account();
-        acc.setAccountNum(1010101010L);
-        acc.setBalanceAmt(1000.0d);
-        acc.setAcctId(2L);
-        this.mockServer.expect(requestTo(baseUrl + "/withdraw"))
-                .andRespond(withStatus(HttpStatus.BAD_REQUEST));
-    }
-
-    @Test
-    public void withdrawAmountMoreThan90PercentExceptionTest() {
-        Account acc = new Account();
-        acc.setAccountNum(1010101010L);
-        acc.setBalanceAmt(1000.0d);
-        acc.setAcctId(2L);
-        this.mockServer.expect(requestTo(baseUrl + "/withdraw"))
-                .andRespond(withStatus(HttpStatus.BAD_REQUEST));
-    }
-
-    @Test
-    public void deleteAccountTest() {
-        Account acc = new Account();
-        acc.setAccountNum(1010101010L);
-        acc.setBalanceAmt(1000.0d);
-        acc.setAcctId(2L);
-        Customer cus = new Customer();
-        cus.setAccounts(Collections.singletonList(acc));
-        cus.setCustName("Vikram");
-        this.mockServer.expect(requestTo(baseUrl + "/account"))
-                .andRespond(withStatus(HttpStatus.OK));
-
-        List<Customer> customerList = customerRepo.findAll();
-        accountRepo.delete(acc);
-        customerRepo.saveAll(customerList);
-
-        assertEquals(2L, customerList.get(0).getAccounts().get(0).getAcctId());
-        assertEquals(1000.0d, customerList.get(0).getAccounts().get(0).getBalanceAmt());
-        assertEquals(10101010101L, customerList.get(0).getCustName());
-    }
-
-    @Test
-    public void deleteAccountInternalServerExceptionTest() {
-        Account acc = new Account();
-        acc.setAccountNum(1010101010L);
-        acc.setBalanceAmt(1000.0d);
-        acc.setAcctId(2L);
-        Customer cus = new Customer();
-        cus.setAccounts(Collections.singletonList(acc));
-        cus.setCustName("Vikram");
-        this.mockServer.expect(requestTo(baseUrl + "/account"))
-                .andRespond(withStatus(HttpStatus.INTERNAL_SERVER_ERROR));
-    }
-
+    /** Following method is used to create a new account */
     @Test
     public void createAccountTest() {
         Account acc = new Account();
@@ -146,23 +56,7 @@ public class BankingTest {
         assertEquals(2L, res.getAccounts().get(0).getAcctId());
     }
 
-    @Test
-    public void createAccountDepositAmtLimitExceptionTest() {
-        Account acc = new Account();
-        acc.setAccountNum(1010101010L);
-        acc.setBalanceAmt(10050.0d);
-        acc.setAcctId(2L);
-        Customer cus = new Customer();
-        cus.setAccounts(Collections.singletonList(acc));
-        cus.setCustName("Vikram");
-
-        this.mockServer.expect(requestTo(baseUrl + "/account"))
-                .andRespond(withStatus(HttpStatus.BAD_REQUEST));
-        Customer response = customerRepo.save(cus);
-        assertEquals("Vikram", response.getCustName());
-        assertEquals(1010101010L, response.getAccounts().get(0).getBalanceAmt());
-    }
-
+    /** Following method is used to get all customer account details */
     @Test
     public void getAllAccountsTest() {
         Account acc = new Account();
@@ -196,4 +90,118 @@ public class BankingTest {
         assertEquals(2020202020L, res.get(2).getAccounts().get(0).getAccountNum());
     }
 
+    /** Following method is used to check the deposit amount limit exception at the time of create Account*/
+    @Test
+    public void createAccountDepositAmtLimitExceptionTest() {
+        Account acc = new Account();
+        acc.setAccountNum(1010101010L);
+        acc.setBalanceAmt(10050.0d);
+        acc.setAcctId(2L);
+        Customer cus = new Customer();
+        cus.setAccounts(Collections.singletonList(acc));
+        cus.setCustName("Vikram");
+
+        this.mockServer.expect(requestTo(baseUrl + "/account"))
+                .andRespond(withStatus(HttpStatus.BAD_REQUEST));
+        Customer response = customerRepo.save(cus);
+        assertEquals("Vikram", response.getCustName());
+        assertEquals(1010101010L, response.getAccounts().get(0).getBalanceAmt());
+    }
+
+    /** Following method is used to deposit amount for a particular account */
+    @Test
+    public void depositAmountTest() {
+        Account acc = new Account();
+        acc.setAccountNum(1010101010L);
+        acc.setBalanceAmt(1000.0d);
+        acc.setAcctId(2L);
+        this.mockServer.expect(requestTo(baseUrl + "/deposit"))
+                .andRespond(withSuccess((Resource) acc, MediaType.APPLICATION_JSON));
+
+        Account res = accountRepo.save(acc);
+        assertEquals(acc.getBalanceAmt(), res.getBalanceAmt());
+        assertEquals(acc.getAcctId(), res.getAcctId());
+    }
+    /** Following method is used to check the deposit amount limit exception */
+    @Test
+    public void depositAmountLimitExceptionTest() {
+        Account acc = new Account();
+        acc.setAccountNum(1010101010L);
+        acc.setBalanceAmt(1000.0d);
+        acc.setAcctId(2L);
+        this.mockServer.expect(requestTo(baseUrl + "/deposit?depositAmount=10001&accountNum=1234567"))
+                .andRespond(withStatus(HttpStatus.BAD_REQUEST));
+    }
+
+    /** Following method is used to withdraw amount from an account */
+    @Test
+    public void withdrawAmountTest() {
+        Account acc = new Account();
+        acc.setAccountNum(1010101010L);
+        acc.setBalanceAmt(1000.0d);
+        acc.setAcctId(2L);
+        this.mockServer.expect(requestTo(baseUrl + "/withdraw?withdrawAmount=10&accountNum=1234567"))
+                .andRespond(withSuccess((Resource) acc, MediaType.APPLICATION_JSON));
+
+        Account res = accountRepo.save(acc);
+        assertEquals(acc.getBalanceAmt() - 200, res.getBalanceAmt());
+        assertEquals(acc.getAcctId(), res.getAcctId());
+    }
+
+    /** Following method is used to check withdraw amount limit exception*/
+    @Test
+    public void withdrawAmountLimitExceptionTest() {
+        Account acc = new Account();
+        acc.setAccountNum(1010101010L);
+        acc.setBalanceAmt(1000.0d);
+        acc.setAcctId(2L);
+        this.mockServer.expect(requestTo(baseUrl + "/withdrawwithdrawAmount=901&accountNum=1234567"))
+                .andRespond(withStatus(HttpStatus.BAD_REQUEST));
+    }
+    /** Following method is used to check the response when withdraw amount is more than 90 percent of the available balance*/
+    @Test
+    public void withdrawAmountMoreThan90PercentExceptionTest() {
+        Account acc = new Account();
+        acc.setAccountNum(1010101010L);
+        acc.setBalanceAmt(1000.0d);
+        acc.setAcctId(2L);
+        this.mockServer.expect(requestTo(baseUrl + "/withdraw/withdrawwithdrawAmount=901&accountNum=1234567"))
+                .andRespond(withStatus(HttpStatus.BAD_REQUEST));
+    }
+
+    /** Following method is used to check the response when account has been deleted*/
+    @Test
+    public void deleteAccountTest() {
+        Account acc = new Account();
+        acc.setAccountNum(1010101010L);
+        acc.setBalanceAmt(1000.0d);
+        acc.setAcctId(2L);
+        Customer cus = new Customer();
+        cus.setAccounts(Collections.singletonList(acc));
+        cus.setCustName("Vikram");
+        this.mockServer.expect(requestTo(baseUrl + "/account"))
+                .andRespond(withStatus(HttpStatus.OK));
+
+        List<Customer> customerList = customerRepo.findAll();
+        accountRepo.delete(acc);
+        customerRepo.saveAll(customerList);
+
+        assertEquals(2L, customerList.get(0).getAccounts().get(0).getAcctId());
+        assertEquals(1000.0d, customerList.get(0).getAccounts().get(0).getBalanceAmt());
+        assertEquals(10101010101L, customerList.get(0).getCustName());
+    }
+
+    /** Following method is used to check the response when internal exception occurs while deleting the user account*/
+    @Test
+    public void deleteAccountInternalServerExceptionTest() {
+        Account acc = new Account();
+        acc.setAccountNum(1010101010L);
+        acc.setBalanceAmt(1000.0d);
+        acc.setAcctId(2L);
+        Customer cus = new Customer();
+        cus.setAccounts(Collections.singletonList(acc));
+        cus.setCustName("Vikram");
+        this.mockServer.expect(requestTo(baseUrl + "/account"))
+                .andRespond(withStatus(HttpStatus.INTERNAL_SERVER_ERROR));
+    }
 }
